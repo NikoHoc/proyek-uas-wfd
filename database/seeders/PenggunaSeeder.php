5810845656ba56f241fa022cc82708fdaad95fa7
Nikolas Henrik Owen <niko.owen88@gmail.com>
2024-11-09 16:48:41 +0700

    add role and pengguna seeder

--- database/seeders/PenggunaSeeder.php
+++ database/seeders/PenggunaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PenggunaSeeder extends Seeder
 {
@@ -12,6 +13,25 @@ class PenggunaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('pengguna')->insert([
+            [
+                'id' => 1,
+                'username' => 'admin',
+                'password' => 'admin',
+                'id_role' => 1
+            ],
+            [
+                'id' => 2,
+                'username' => 'Adit',
+                'password' => 1234,
+                'id_role' => 2
+            ],
+            [
+                'id' => 3,
+                'username' => 'Budi',
+                'password' => 1234,
+                'id_role' => 3
+            ]
+        ]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RoleSeeder extends Seeder
 {
@@ -12,6 +13,19 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('role')->insert([
+            [
+                'id' => 1,
+                'nama' => 'admin'
+            ],
+            [
+                'id' => 2,
+                'nama' => 'pemilik'
+            ],
+            [
+                'id' => 3,
+                'nama' => 'penghuni'
+            ]
+        ]);
     }
 }
